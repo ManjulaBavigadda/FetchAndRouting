b4d684f3337c2a83477a897a9cde5398008c464a
ManjulaBavigadda <manjulabavigadda@gmail.com>
2024-12-15 06:17:39 +0000

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import UserInfo from '../UserInfo'
+import BlogsList from '../BlogList'
+import './index.css'
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogsList />
+  </div>
+)
+export default Home
